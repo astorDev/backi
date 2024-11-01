b5f6c5a38022ef0e48bec721a27ae4c107a61ddf
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-01 12:02:36 +0400

    mediatr package

---
+++ mediator/dotnet/lib/ServiceScopeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Backi;
+
+public static class ServiceScopeExtensions
+{
+    public static async Task SendMediatorRequest<TRequest>(this IServiceScopeFactory serviceScopeFactory, ILogger? logger = null, CancellationToken? cancellationToken = null)
+        where TRequest : IRequest
+    {
+        logger?.LogDebug("Sending {requestType} to mediator in a new scope", typeof(TRequest));
+        using var scope = serviceScopeFactory.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+        var request = (TRequest)Activator.CreateInstance(typeof(TRequest))!;
+        await mediator.Send(request, cancellationToken ?? CancellationToken.None);
+        logger?.LogInformation("{requestType} processed by mediator", typeof(TRequest));
+    }
+}
