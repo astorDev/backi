b00fb969df9802898e49bc0fe674128604a5cab4
Egor Tarasov <pavluchenk0@yandex.ru>
2025-03-06 16:28:37 +0300

    backi postgres playground (#13)

---
+++ postgres/playground/BasicShould.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace Backi.Postgres.Playground;
+
+[TestClass]
+public sealed class BasicShould
+{
+    [TestMethod]
+    public async Task ConnectAndPerformBasicOperations()
+    {
+        using var db = await Db.CreateEmpty();
+
+        db.Queue.Add(new QueueItem { Name = "Test" });
+        await db.SaveChangesAsync();
+
+        var count = await db.Queue.CountAsync();
+        count.ShouldBe(1);
+    }
+}
