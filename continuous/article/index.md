74a759917bd6d1dde48f89b2b4f44562578d22cb
Egor Tarasov <vosarat1995+1@gmail.com>
2025-03-18 18:45:08 +0300

    Backi.Continuous Init (#19)

---
+++ continuous/article/index.md
@@ -0,0 +1,3 @@
+## How to Implement a .NET Background Service for Continuous Work in 2025
+
+> Implementing an Ever-Running Background Service utilizing C# 11 feature - static virtual members in interfaces
---
+++ continuous/lib/ContinuousBackgroundService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Backi;
+
+public interface IContinuousWorkIteration
+{
+    Task Run(CancellationToken stoppingToken);
+    abstract static Task OnException(Exception ex, ILogger logger);
+}
+
+public class ContinuousBackgroundService<TIteration>(IServiceScopeFactory scopeFactory, ILogger<TIteration> logger) 
+    : BackgroundService 
+    where TIteration : IContinuousWorkIteration
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                await using var scope = scopeFactory.CreateAsyncScope();
+                var iteration = scope.ServiceProvider.GetRequiredService<TIteration>();
+                
+                await iteration.Run(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                await TIteration.OnException(ex, logger);
+            }
+        }
+    }
+}
+
+public static partial class Registration
+{
+    public static IServiceCollection AddContinuousBackgroundService<TIteration>(this IServiceCollection services)
+        where TIteration : class, IContinuousWorkIteration
+    {
+        services.AddScoped<TIteration>();
+        services.AddHostedService<ContinuousBackgroundService<TIteration>>();
+        return services;
+    }
+}
